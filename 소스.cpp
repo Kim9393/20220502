2af0fc910cbd233b5780e4565fd49e2198406827
KIM SUHAN <egl84@naver.com>
2022-05-02 09:51:59 +0900

    av

---
+++ 소스.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//정수형 변수 사칙연산
+
+	int a = 2;
+	int b = 3;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+
+	//실수형 변수 사칙연산
+
+	using namespace std;
+
+	float kim = 1.2;
+	float lee = 2.3;
+
+	cout << kim + lee << endl;
+	cout << kim - lee << endl;
+	cout << kim * lee << endl;
+	cout << kim / lee << endl;
+	
+
+	
+	return 0;
+}
